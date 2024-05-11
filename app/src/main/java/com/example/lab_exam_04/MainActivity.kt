9cfcc3924b11a4a1e127c65de31c4869c00d3296
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-05-11 09:15:28 +0530

    created ui functinality

--- app/src/main/java/com/example/lab_exam_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_exam_04/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.lab_exam_04
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.lab_exam_04.databinding.ActivityMainBinding
+import com.example.lab_exam_04.util.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialogBox : Dialog by lazy {
+        Dialog(this,R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialogBox : Dialog by lazy {
+        Dialog(this,R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+        val addCloseImgBtn = addTaskDialogBox.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImgBtn = updateTaskDialogBox.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImgBtn.setOnClickListener { addTaskDialogBox.dismiss() }
+        updateCloseImgBtn.setOnClickListener { updateTaskDialogBox.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialogBox.show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/lab_exam_04/util/util.kt
@@ -0,0 +1,13 @@
+package com.example.lab_exam_04.util
+
+import android.app.Dialog
+import android.widget.LinearLayout
+
+fun Dialog.setupDialog(layoutResId:Int){
+    setContentView(layoutResId)
+    window!!.setLayout(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    setCancelable(false)
+}
